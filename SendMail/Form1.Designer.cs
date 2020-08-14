58d3c1a0f1b9538f79dfd6f4befe8f2fffd6e8b1
Glenn Alon <glennalon216@aol.com>
2020-08-14 13:11:07 +0800

    updated copyright notice

--- SendMail/Form1.Designer.cs
+++ SendMail/Form1.Designer.cs
@@ -1,5 +1,6 @@
 ﻿#region Apache License
-//  Copyright 2018 Glenn Alon
+
+//  Copyright 2018-2020 Glenn Alon
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -12,6 +13,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+
 #endregion
 
 
--- SendMail/Program.cs
+++ SendMail/Program.cs
@@ -1,6 +1,6 @@
 ﻿#region Apache License
 
-//  Copyright 2018 Glenn Alon
+//  Copyright 2018-2020 Glenn Alon
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
